f673f02fb7b381a1d36ab9d6753517b6efba9662
JikoUnderscore <j1k0@mail.bg>
2025-07-03 23:25:59 +0300

    a new life

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,107 @@
 #![allow(non_camel_case_types)]
 
-use std::collections::HashSet;
+use std::{collections::HashSet, num::NonZeroU32};
 
-// const DIGITS: u8 = 9;              // 1, 2, 3, 4, 5, 6, 7, 8, 9
-// const DIGIT_POSISTIONS: u8 = 4; // ----
 const SIZE: usize = 6561;
 
-// const SECRET: (u8, u8, u8, u8) = (5, 9, 1, 4);
+#[derive(Debug, Clone)]
+pub struct NonZeroNums32(NonZeroU32);
+const _: () = assert!(size_of::<Option<NonZeroNums32>>() == size_of::<u32>());
+
+impl NonZeroNums32 {
+    pub const fn new(a: u8, b: u8, c: u8, d: u8) -> Option<Self> {
+        let val = u32::from_be_bytes([a, b, c, d]);
+        return match NonZeroU32::new(val) {
+            Some(x) => Some(Self(x)),
+            None => None,
+        };
+    }
+
+    pub fn new_none_zero(a: u8, b: u8, c: u8, d: u8) -> Self {
+        let val = u32::from_be_bytes([a, b, c, d]);
+        return NonZeroNums32(NonZeroU32::new(val).expect("to be non zero"));
+    }
+
+    pub fn get(self) -> (u8, u8, u8, u8) {
+        self.0.get().to_be_bytes().into()
+    }
+}
+
+struct ChoseNum {
+    data: HashSet<[u32; 15]>,
+}
+
+impl ChoseNum {
+    pub fn new() -> Self {
+        Self { data: HashSet::with_capacity(15) } //TODO: find a better cap
+    }
+
+    fn choose_number(&mut self, guesses: &[Option<NonZeroNums32>]) -> NonZeroNums32 {
+        let mut table: [[u32; 15]; SIZE] = [[0; 15]; SIZE];
+
+        for (i, r) in guesses.iter().enumerate() {
+            if let Some(r) = r {
+                for l in guesses.iter().flatten() {
+                    let bull_cow = play_bull_cows(r.clone(), l.clone());
+                    match bull_cow {
+                        0 => table[i][0] += 1,
+                        1 => table[i][1] += 1,
+                        2 => table[i][2] += 1,
+                        3 => table[i][3] += 1,
+                        4 => table[i][4] += 1,
+                        10 => table[i][5] += 1,
+                        11 => table[i][6] += 1,
+                        12 => table[i][7] += 1,
+                        13 => table[i][8] += 1,
+                        20 => table[i][9] += 1,
+                        21 => table[i][10] += 1,
+                        22 => table[i][11] += 1,
+                        31 => table[i][12] += 1,
+                        30 => table[i][13] += 1,
+                        40 => table[i][14] += 1,
+                        _ => unsafe {
+                            std::hint::unreachable_unchecked();
+                        },
+                    }
+                }
+            }
+        }
+
+        let mut m = u32::MAX;
+        let mut numer_index = 0;
+        for (i, a) in table.iter().enumerate() {
+            let new_max = *a.iter().max().unwrap();
+            if guesses[i].is_some() && (new_max < m) {
+                m = new_max;
+                numer_index = i;
+            }
+        }
+
+        self.data.clear();
+        for (i, guess) in guesses.iter().enumerate() {
+            if let Some(gu) = guess {
+                if not!(self.data.contains(&table[i])) {
+                    self.data.insert(table[i]);
+                    println!("index: {}={:?},  {:?}", i, gu.clone().get(), table[i]);
+                }
+            }
+        }
+        return guesses[numer_index].clone().expect("to be non zero");
+    }
+}
 
 // https://www.youtube.com/watch?v=FR_71HyBytE&t=846s
 fn main() {
     let secret = match handle_cla() {
-        Ok(e) => { e }
-        Err(_) => { return; }
+        Ok(e) => e,
+        Err(_) => {
+            NonZeroNums32::new_none_zero(5, 8, 7, 6) /* return; */
+        }
     };
 
-    let mut guesses = make_array();
+    static mut ARRAY: [Option<NonZeroNums32>; SIZE] = make_array();
+    let guesses = unsafe { &mut (*(&raw mut ARRAY)) };
+    let mut genn = ChoseNum::new();
 
     let mut answare_index = 0;
     let mut chosen_number;
@@ -24,24 +110,23 @@ fn main() {
     let mut run = true;
     while run {
         println!("---------------------");
-        chosen_number = choose_number_gen(&guesses);
-        println!("\nchosen_number {:?}", chosen_number);
-        compare_numbers(&mut run, chosen_number.unwrap(), secret, &mut guesses);
+        chosen_number = genn.choose_number(guesses);
+        println!("\nchosen_number {:?}", chosen_number.clone().get());
+        compare_numbers(&mut run, chosen_number, secret.clone(), guesses);
         println!();
-        answare_index = found_answare(&mut run, &guesses);
-
+        answare_index = found_answare(&mut run, guesses);
 
         luup += 1;
     }
     println!("---------------------");
     println!("loops {:?}", luup);
-    println!("SECRET {:?}", secret);
-    println!("answare {:?}", guesses[answare_index]);
+    println!("SECRET {:?}", secret.get());
+    println!("answare {:?}", guesses[answare_index].clone().unwrap().get());
 }
 
-const fn make_array() -> [Option<(u8, u8, u8, u8)>; SIZE] {
+const fn make_array() -> [Option<NonZeroNums32>; SIZE] {
     const DIGITS: u8 = 9;
-    let mut arr = [None; SIZE];
+    let mut arr: [Option<NonZeroNums32>; SIZE] = [const { None }; SIZE];
     let mut n = 0;
 
     let mut h = 1;
@@ -55,7 +140,7 @@ const fn make_array() -> [Option<(u8, u8, u8, u8)>; SIZE] {
             while d <= DIGITS {
                 e = 1;
                 while e <= DIGITS {
-                    arr[n] = Some((h, s, d, e));
+                    arr[n] = NonZeroNums32::new(h, s, d, e);
                     n += 1;
 
                     e += 1;
@@ -70,7 +155,7 @@ const fn make_array() -> [Option<(u8, u8, u8, u8)>; SIZE] {
     return arr;
 }
 
-fn found_answare(run: &mut bool, guesses: &[Option<(u8, u8, u8, u8)>; SIZE]) -> usize {
+fn found_answare(run: &mut bool, guesses: &[Option<NonZeroNums32>]) -> usize {
     let mut n = 0;
     let mut index = 0;
     for (i, guess) in guesses.iter().enumerate() {
@@ -89,81 +174,38 @@ fn found_answare(run: &mut bool, guesses: &[Option<(u8, u8, u8, u8)>; SIZE]) -> 
     return index;
 }
 
-fn compare_numbers(run: &mut bool, guess: (u8, u8, u8, u8), secret: (u8, u8, u8, u8), guesses: &mut [Option<(u8, u8, u8, u8)>; SIZE]) {
+fn compare_numbers(run: &mut bool, guess: NonZeroNums32, secret: NonZeroNums32, guesses: &mut [Option<NonZeroNums32>]) {
     let mut witch = [true; 15];
 
-    let bull_cow = play_bull_cows(&guess, &secret);
-
+    let bull_cow = play_bull_cows(guess.clone(), secret);
+    println!("{:02}", bull_cow);
     match bull_cow {
-        0 => {
-            witch[0] = false;
-            println!("0 0");
-        },
-        1 => {
-            witch[1] = false;
-            println!("0 1");
-        },
-        2 => {
-            witch[2] = false;
-            println!("0 2");
-        },
-        3 => {
-            witch[3] = false;
-            println!("0 3");
-        },
-        4 => {
-            witch[4] = false;
-            println!("0 4");
-        },
-        10 => {
-            witch[5] = false;
-            println!("1 0");
-        },
-        11 => {
-            witch[6] = false;
-            println!("1 1");
-        },
-        12 => {
-            witch[7] = false;
-            println!("1 2");
-        },
-        13 => {
-            witch[8] = false;
-            println!("1 3");
-        },
-        20 => {
-            witch[9] = false;
-            println!("2 0");
-        },
-        21 => {
-            witch[10] = false;
-            println!("2 1");
-        },
-        22 => {
-            witch[11] = false;
-            println!("2 2");
-        },
-        31 => {
-            witch[12] = false;
-            println!("3 1");
-        },
-        30 => {
-            witch[13] = false;
-            println!("3 0");
-        },
+        0 => witch[0] = false,
+        1 => witch[1] = false,
+        2 => witch[2] = false,
+        3 => witch[3] = false,
+        4 => witch[4] = false,
+        10 => witch[5] = false,
+        11 => witch[6] = false,
+        12 => witch[7] = false,
+        13 => witch[8] = false,
+        20 => witch[9] = false,
+        21 => witch[10] = false,
+        22 => witch[11] = false,
+        31 => witch[12] = false,
+        30 => witch[13] = false,
         40 => {
             witch[14] = false;
-            println!("4 0");
             *run = false;
+        }
+        _ => unsafe {
+            std::hint::unreachable_unchecked();
         },
-        _ => {}
     }
 
-
     for og in guesses.iter_mut() {
-        if og.is_some() {
-            let g = og.unwrap();
-            let bull_cow = play_bull_cows(&guess, &g);
+        if let Some(g) = og {
+            let bull_cow = play_bull_cows(guess.clone(), g.clone());
 
             match bull_cow {
                 0 if witch[0] => *og = None,
@@ -181,67 +223,18 @@ fn compare_numbers(run: &mut bool, guess: (u8, u8, u8, u8), secret: (u8, u8, u8,
                 31 if witch[12] => *og = None,
                 30 if witch[13] => *og = None,
                 40 if witch[14] => *og = None,
-                _ => {}
+                _ => unsafe {
+                    std::hint::unreachable_unchecked();
+                },
             }
         }
     }
 }
 
-fn choose_number_gen(guesses: &[Option<(u8, u8, u8, u8)>; SIZE]) -> Option<(u8, u8, u8, u8)> {
-    let mut table: [[u32; 15]; SIZE] = [[0; 15]; SIZE];
+fn play_bull_cows(guess: NonZeroNums32, secret: NonZeroNums32) -> u8 {
+    let guess = guess.get();
+    let secret = secret.get();
 
-
-    for (i, r) in guesses.iter().enumerate() {
-        if let Some(r) = r {
-            for l in guesses.iter().flatten() {
-                let bull_cow = play_bull_cows(r, l);
-                match bull_cow {
-                    0 => table[i][0] += 1,
-                    1 => table[i][1] += 1,
-                    2 => table[i][2] += 1,
-                    3 => table[i][3] += 1,
-                    4 => table[i][4] += 1,
-                    10 => table[i][5] += 1,
-                    11 => table[i][6] += 1,
-                    12 => table[i][7] += 1,
-                    13 => table[i][8] += 1,
-                    20 => table[i][9] += 1,
-                    21 => table[i][10] += 1,
-                    22 => table[i][11] += 1,
-                    31 => table[i][12] += 1,
-                    30 => table[i][13] += 1,
-                    40 => table[i][14] += 1,
-                    _ => {}
-                }
-            }
-        }
-    }
-
-    let mut m = u32::MAX;
-    let mut numer_index = 0;
-    for (i, a) in table.iter().enumerate() {
-        let new_max = *a.iter().max().unwrap();
-        if guesses[i].is_some() && (new_max < m) {
-            m = new_max;
-            numer_index = i;
-        }
-    }
-
-    let mut unic = HashSet::new();
-    for (i, guess) in guesses.iter().enumerate() {
-        if let Some(gu) = guess {
-            if not!(unic.contains(&table[i])) {
-                unic.insert(table[i]);
-                println!("index: {}={:?},  {:?}", i, gu, table[i]);
-            }
-        }
-    }
-    assert_eq!(numer_index, SIZE);
-
-    return guesses[numer_index];
-}
-
-fn play_bull_cows(guess: &(u8, u8, u8, u8), secret: &(u8, u8, u8, u8)) -> u8 {
     let mut bulls = 0;
     let mut cows = 0;
 
@@ -258,83 +251,56 @@ fn play_bull_cows(guess: &(u8, u8, u8, u8), secret: &(u8, u8, u8, u8)) -> u8 {
         bulls += 1;
     }
 
-    if guess.0 != secret.0 &&
-        (guess.0 == secret.1 ||
-            guess.0 == secret.2 ||
-            guess.0 == secret.3) {
+    if guess.0 != secret.0 && (guess.0 == secret.1 || guess.0 == secret.2 || guess.0 == secret.3) {
         cows += 1;
     }
-    if guess.1 != secret.1 &&
-        (guess.1 == secret.0 ||
-            guess.1 == secret.2 ||
-            guess.1 == secret.3) {
+    if guess.1 != secret.1 && (guess.1 == secret.0 || guess.1 == secret.2 || guess.1 == secret.3) {
         cows += 1;
     }
-    if guess.2 != secret.2 &&
-        (guess.2 == secret.1 ||
-            guess.2 == secret.0 ||
-            guess.2 == secret.3) {
+    if guess.2 != secret.2 && (guess.2 == secret.1 || guess.2 == secret.0 || guess.2 == secret.3) {
         cows += 1;
     }
-    if guess.3 != secret.3 &&
-        (guess.3 == secret.0 ||
-            guess.3 == secret.1 ||
-            guess.3 == secret.2) {
+    if guess.3 != secret.3 && (guess.3 == secret.0 || guess.3 == secret.1 || guess.3 == secret.2) {
         cows += 1;
     }
-
 
     return bulls * 10 + cows;
 }
 
-
-type void = ();
-
-#[allow(non_upper_case_globals)]
-const void: () = ();
-
-
-fn handle_cla() -> Result<(u8, u8, u8, u8), void> {
+fn handle_cla() -> Result<NonZeroNums32, ()> {
     let args: Vec<String> = std::env::args().collect();
 
-    // println!("{:?}", args);
-    // assert!(args.len() >= 2, "Pass a number");
     if not!(args.len() >= 2) {
-        println!("Enter a foure digit number as a commadn line argumen");
-        return Err(void);
+        eprintln!("Enter a foure digit number as a commadn line argumen");
+        return Err(());
     }
 
-
-    // println!("args[1].len() {:?}", args[1].len());
-    // assert_eq!(args[1].len(), 4, "arg should be 4 char long");
     if not!(args[1].len() == 4) {
-        println!("Number should be 4 char long!");
-        return Err(void);
+        eprintln!("Number should be 4 char long!");
+        return Err(());
     }
-    // println!("args[1] {:?}", args[1]);
 
     let ss = args[1].as_bytes();
-    // println!("{:?} {:?} {:?} {:?}", ss[0] as char, ss[1]as char, ss[2]as char, ss[3]as char);
 
-    let h = (ss[0] as char).to_digit(10).unwrap();
-    let s = (ss[1] as char).to_digit(10).unwrap();
-    let d = (ss[2] as char).to_digit(10).unwrap();
-    let e = (ss[3] as char).to_digit(10).unwrap();
+    let h = (ss[0] as char).to_digit(10).expect("to be a numebr");
+    let s = (ss[1] as char).to_digit(10).expect("to be a numebr");
+    let d = (ss[2] as char).to_digit(10).expect("to be a numebr");
+    let e = (ss[3] as char).to_digit(10).expect("to be a numebr");
 
-    // assert!(h > 0 || s > 0 || d > 0 || e > 0, "Numbers less than zero are not an allowed");
-    // assert!(h <= 9 || s <= 9 || d <= 9 || e <= 9, "Some error !");
-    if not!(h > 0 || s > 0 || d > 0 || e > 0) {
-        println!("Numbers should be more than zero");
-        return Err(void);
+    if not! {h > 0 || s > 0 || d > 0 || e > 0} {
+        eprintln!("Numbers should be more than zero");
+        return Err(());
     }
     if not!(h <= 9 || s <= 9 || d <= 9 || e <= 9) {
-        println!("Numbers should be less than nine");
-        return Err(void);
+        eprintln!("Numbers should be less than nine");
+        return Err(());
     }
-    return Ok((h as u8, s as u8, d as u8, e as u8));
+    return Ok(NonZeroNums32::new_none_zero(h as u8, s as u8, d as u8, e as u8));
 }
 
 #[macro_export]
 macro_rules! not {
-    ($x:expr) => {!$x};
+    ($x:expr) => {
+        !$x
+    };
 }
